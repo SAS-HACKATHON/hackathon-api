0ef10a5dbb102e5fe0e454163a333f9b6634d3ff
Sofien MNASSRI <soofinco.m@gmail.com>
2017-09-21 15:26:50 +0100

    fix User

--- src/main/java/com/sofrecom/hackathon/model/User.java
+++ src/main/java/com/sofrecom/hackathon/model/User.java
@@ -9,76 +9,70 @@ import java.util.EnumSet;
 
 @Entity
 public class User {
-    @Id
-      private String userId;
-      private String password = "";
-      private String company;
-      private String firstName;
-      private String lastName;
-      private String email;
+	@Id
+	private String userId;
+	private String password = "";
+	private String company;
+	private String firstName;
+	private String lastName;
+	private String email;
 
-    @JsonIgnore   private int    securityProviderId;
-    @JsonIgnore   private int    defaultCustomerId;
+	@JsonIgnore
+	private int securityProviderId;
+	@JsonIgnore
+	private int defaultCustomerId;
 
-    @JsonIgnore   private String phone;
-    @JsonIgnore   private String address1;
-    @JsonIgnore   private String address2;
-    @JsonIgnore   private String country;
-    @JsonIgnore   private String postal;
+	@Enumerated(EnumType.STRING)
+	private Role role;
 
-    @Enumerated(EnumType.STRING)
-      private Role role;
+	@JsonIgnore
+	private boolean isActive;
+	@JsonIgnore
+	private boolean isBlocked;
+	@JsonIgnore
+	private String secretQuestion;
+	@JsonIgnore
+	private String secretAnswer;
+	@JsonIgnore
+	private boolean enableBetaTesting;
+	@JsonIgnore
+	private boolean enableRenewal;
 
-    //@JsonIgnore
-    @JsonIgnore   private boolean isActive;
-    //@JsonIgnore
-    @JsonIgnore  private boolean isBlocked;
-    @JsonIgnore  private String  secretQuestion;
-    @JsonIgnore  private String  secretAnswer;
-    @JsonIgnore  private boolean enableBetaTesting;
-    @JsonIgnore  private boolean enableRenewal;
+	public User() {
+		this("new", "PASSWORD", Role.USER, "new", "new", true, "", "", true, false);
+	}
 
-    public User(){
-        this("new", "PASSWORD", Role.USER, "new", "new", true, "", "", "", "", "", "", "", "", true, false);
-    }
+	public User(String userId, String password, String firstName, String lastName) {
+		this(userId, password, Role.USER, firstName, lastName, true, "", "", true, false);
+	}
 
-    public User(String userId, String password, String firstName, String lastName){
-        this(userId, password, Role.USER, firstName, lastName, true, "", "", "", "", "", "", "", "", true, false);
-    }
+	public User(String userId, String password, Role role, String firstName, String lastName) {
+		this(userId, password, role, firstName, lastName, true, "", "", true, false);
+	}
 
-    public User(String userId, String password, Role role, String firstName, String lastName){
-        this(userId, password, role, firstName, lastName, true, "", "", "", "", "", "", "", "", true, false);
-    }
+	public User(String userId, String password, Role role, String firstName, String lastName, boolean isActive) {
+		this(userId, password, role, firstName, lastName, isActive, "", "", true, false);
+	}
 
-    public User(String userId, String password, Role role, String firstName, String lastName, boolean isActive){
-        this(userId, password, role, firstName, lastName, isActive, "", "", "", "", "", "", "", "", true, false);
-    }
+	public User(String userId, String password, Role role, String firstName, String lastName, boolean isActive,
+			String secretQuestion, String secretAnswer, boolean enableRenewal, boolean enableBetaTesting) {
+		this.setUserId(userId);
+		this.setEmail(userId);
+		this.setPassword(new BCryptPasswordEncoder().encode(password));
+		this.setRole(role);
+		this.setFirstName(firstName);
+		this.setLastName(lastName);
+		this.setActive(isActive);
 
-    public User(String userId, String password, Role role, String firstName, String lastName, boolean isActive,
-         String company, String phone, String address1, String address2, String country, String postal,
-         String secretQuestion, String secretAnswer, boolean enableRenewal, boolean enableBetaTesting){
-        this.setUserId(userId);
-        this.setEmail(userId);
-        this.setPassword(new BCryptPasswordEncoder().encode(password));
-        this.setRole(role);
-        this.setFirstName(firstName);
-        this.setLastName(lastName);
-        this.setActive(isActive);
-        this.setCompany(company);
-        this.setPhone(phone);
-        this.setAddress1(address1);
-        this.setAddress2(address2);
-        this.setCountry(country);
-        this.setPostal(postal);
-        this.setSecretQuestion(secretQuestion);
-        this.setSecretAnswer(secretAnswer);
-        this.setEnableRenewal(enableRenewal);
-        this.setEnableBetaTesting(enableBetaTesting);
-    }
+		this.setSecretQuestion(secretQuestion);
+		this.setSecretAnswer(secretAnswer);
+		this.setEnableRenewal(enableRenewal);
+		this.setEnableBetaTesting(enableBetaTesting);
+	}
 
-    public String getFullName(){
-        return this.firstName + this.lastName;
-    }
+	public String getFullName() {
+		return this.firstName + this.lastName;
+	}
 
 	public String getUserId() {
 		return userId;
@@ -144,46 +138,6 @@ public class User {
 		this.defaultCustomerId = defaultCustomerId;
 	}
 
-	public String getPhone() {
-		return phone;
-	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-
-	public String getAddress1() {
-		return address1;
-	}
-
-	public void setAddress1(String address1) {
-		this.address1 = address1;
-	}
-
-	public String getAddress2() {
-		return address2;
-	}
-
-	public void setAddress2(String address2) {
-		this.address2 = address2;
-	}
-
-	public String getCountry() {
-		return country;
-	}
-
-	public void setCountry(String country) {
-		this.country = country;
-	}
-
-	public String getPostal() {
-		return postal;
-	}
-
-	public void setPostal(String postal) {
-		this.postal = postal;
-	}
-
 	public Role getRole() {
 		return role;
 	}
@@ -239,6 +193,5 @@ public class User {
 	public void setEnableRenewal(boolean enableRenewal) {
 		this.enableRenewal = enableRenewal;
 	}
-    
-    
+
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,12 +1,12 @@
 /* User */
-insert into user (user_id, password, first_name, last_name, email, company, phone, address1, address2, country, postal, role, is_active, is_blocked, security_provider_id, default_customer_id, secret_question, secret_answer, enable_beta_testing, enable_renewal) values
-('demo'      , 'demo'     , 'Mrinmoy'  , 'Majumdar', 'arivera2@joomla.org'    , 'Abshire Inc', '7-(740)701-4547', '80429 Garrison Crossing', '4967'               , 'USA'        , '64890', 'USER' , 1, 0, 10001, 20000, 'Diverse'       , 'Yellow' , 0, 0),
-('admin'     , 'admin'    , 'Theresa'  , 'Russell' , 'trussell1@about.me'     , 'Glover, Adams and Bins', '383-(779)851-3208', '30874 Graceland Terrace', '99152' , 'USA'        , '51065', 'ADMIN', 1, 0, 10001, 20000, 'knowledge base', 'Mauv'   , 1, 0),
-('user'      , 'user'     , 'Virginia' , 'Reynolds', 'vreynolds0@slashdot.org', 'Rippin, Osinski and Beatty', '84-(228)809-9998', '0118 Burrows Plaza', '496'     , 'USA'        , '94086', 'USER' , 1, 0, 10001, 20000, 'Innovative'    , 'Turquoise', 1, 1),
-('jowens3'   , 'jowens3'  , 'Judy'     , 'Owens'   , 'jowens3@hp.com'         , 'Altenwerth, Fisher and Heidenreich', '30-(772)268-8227', '98 Loeprich Way', '447', 'Greece'     , null   , 'USER' , 0, 0, 10001, 20001, 'capacity'      , 'Fuscia' , 1, 1),
-('kburns4'   , 'kburns4'  , 'Kelly'    , 'Burns'   , 'kburns4@icio.us'        , 'McCullough-Morar', '86-(857)185-5740', '1638 Basil Alley', '56297'               , 'China'      , null   , 'ADMIN', 1, 0, 10000, 20002, 'user-facing'   , 'Crimson', 1, 1),
-('jshaw5'    , 'jshaw5'   , 'Julie'    , 'Shaw'    , 'jshaw5@opera.com'       , 'Steuber-Okuneva', '1-(871)375-6188', '389 Myrtle Pass', '41444'                  , 'Canada'     , null   , 'ADMIN', 1, 1, 10000, 20000, 'software'      , 'Green'  , 0, 1),
-('pgilbert6' , 'pgilbert6', 'Peter'    , 'Gilbert' , 'pgilbert6@eepurl.com'   , 'Robel Inc', '52-(372)555-4687', '11522 Fuller Avenue', '5'                       , 'Mexico'     , '39230', 'ADMIN', 1, 1, 10000, 20000, 'multi-state'   , 'Puce'   , 1, 1),
-('jjacobs7'  , 'jjacobs7' , 'Justin'   , 'Jacobs'  , 'jjacobs7@google.co.uk'  , 'Harris-Bashirian', '963-(199)359-2552', '95012 Hanover Street', '2377'           , 'India'      , null   , 'USER' , 1, 0, 10000, 20002, 'motivating'    , 'Crimson', 1, 0),
-('kbennett8' , 'kbennett8', 'Kevin'    , 'Bennett' , 'kbennett8@hostgator.com', 'Leannon Inc', '62-(892)710-5713', '459 Coleman Drive', '397'                     , 'Indonesia'  , null   , 'ADMIN', 0, 0, 10001, 20000, 'Exclusive'     , 'Purple' , 1, 1),
-('cmurphy9'  , 'cmurphy9' , 'Chris'    , 'Murphy'  , 'cmurphy9@over-blog.com' , 'Mosciski LLC', '64-(272)961-0086', '2 Ludington Point', '7'                      , 'New Zealand', null   , 'ADMIN', 0, 1, 10000, 20000, 'empowering'    , 'Maroon' , 0, 1);
+insert into user (user_id, password, first_name, last_name, email, role, is_active, is_blocked, security_provider_id, default_customer_id, secret_question, secret_answer, enable_beta_testing, enable_renewal) values
+('demo'      , 'demo'     , 'Mrinmoy'  , 'Majumdar', 'arivera2@joomla.org'    , 'USER' , 1, 0, 10001, 20000, 'Diverse'       , 'Yellow' , 0, 0),
+('admin'     , 'admin'    , 'Theresa'  , 'Russell' , 'trussell1@about.me'     , 'ADMIN', 1, 0, 10001, 20000, 'knowledge base', 'Mauv'   , 1, 0),
+('user'      , 'user'     , 'Virginia' , 'Reynolds', 'vreynolds0@slashdot.org','USER' , 1, 0, 10001, 20000, 'Innovative'    , 'Turquoise', 1, 1),
+('jowens3'   , 'jowens3'  , 'Judy'     , 'Owens'   , 'jowens3@hp.com'         , 'USER' , 0, 0, 10001, 20001, 'capacity'      , 'Fuscia' , 1, 1),
+('kburns4'   , 'kburns4'  , 'Kelly'    , 'Burns'   , 'kburns4@icio.us'        , 'ADMIN', 1, 0, 10000, 20002, 'user-facing'   , 'Crimson', 1, 1),
+('jshaw5'    , 'jshaw5'   , 'Julie'    , 'Shaw'    , 'jshaw5@opera.com'      , 'ADMIN', 1, 1, 10000, 20000, 'software'      , 'Green'  , 0, 1),
+('pgilbert6' , 'pgilbert6', 'Peter'    , 'Gilbert' , 'pgilbert6@eepurl.com'   , 'ADMIN', 1, 1, 10000, 20000, 'multi-state'   , 'Puce'   , 1, 1),
+('jjacobs7'  , 'jjacobs7' , 'Justin'   , 'Jacobs'  , 'jjacobs7@google.co.uk'  ,'USER' , 1, 0, 10000, 20002, 'motivating'    , 'Crimson', 1, 0),
+('kbennett8' , 'kbennett8', 'Kevin'    , 'Bennett' , 'kbennett8@hostgator.com' , 'ADMIN', 0, 0, 10001, 20000, 'Exclusive'     , 'Purple' , 1, 1),
+('cmurphy9'  , 'cmurphy9' , 'Chris'    , 'Murphy'  , 'cmurphy9@over-blog.com'  , 'ADMIN', 0, 1, 10000, 20000, 'empowering'    , 'Maroon' , 0, 1);

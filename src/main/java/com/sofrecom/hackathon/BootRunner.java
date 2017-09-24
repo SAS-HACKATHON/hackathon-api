c6ee46426eb9bcc1464142a709aeebe0b0b0cc68
Med Amine Khemiri <khemiri.mohamedamine@gmail.com>
2017-09-24 02:26:22 +0100

    notification websocket

--- src/main/java/com/sofrecom/hackathon/BootRunner.java
+++ src/main/java/com/sofrecom/hackathon/BootRunner.java
@@ -5,20 +5,29 @@ import java.net.UnknownHostException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Import;
 import org.springframework.core.env.Environment;
 import org.springframework.transaction.annotation.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = { "com.sofrecom.hackathon.repository" })
 @EntityScan(basePackages = { "com.sofrecom.hackathon.model" })
 @EnableTransactionManagement
-
-public class BootRunner extends SpringBootServletInitializer {
+@EnableWebSocket
+@EnableScheduling
+public class BootRunner extends SpringBootServletInitializer implements WebSocketConfigurer{
 
 	private static final Logger log = LoggerFactory.getLogger(BootRunner.class);
 
@@ -27,14 +36,22 @@ public class BootRunner extends SpringBootServletInitializer {
 		SpringApplication app = new SpringApplication(BootRunner.class);
 		Environment env = app.run(args).getEnvironment();
 
-		log.info("\n\n${project.base.dir}***********************************************************\n\t"
+		log.info("\n${project.base.dir}\n***********************************************************\n\t"
 				+ "Application '{}' is running! Access URLs:\n\t" + "Local: \t\thttp://localhost:{}{}\n\t"
 				+ "External: \thttp://{}:{}{}" + "\n***********************************************************\n\n",
 				env.getProperty("spring.application.name"), env.getProperty("server.port"),
 				env.getProperty("server.contextPath"), InetAddress.getLocalHost().getHostAddress(),
 				env.getProperty("server.port"), env.getProperty("server.contextPath"));
 	}
+	
+	
+	@Autowired
+    ArticleNotificationHandler articleNotificationHandler;
 
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(articleNotificationHandler, "/counter").setAllowedOrigins("*");
+    }
 }
 
 /*
